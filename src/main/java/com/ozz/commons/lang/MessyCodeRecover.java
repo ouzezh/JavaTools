2abe8efe17ccea8723f6bdd3c1530f8e127af433
ouzezhou <ouzezhou@xdf.cn>
2017-08-23 18:26:52 +0800

    messy code

--- src/main/java/com/ozz/commons/lang/MessyCodeRecover.java
+++ src/main/java/com/ozz/commons/lang/MessyCodeRecover.java
@@ -4,30 +4,64 @@ import java.io.UnsupportedEncodingException;
 
 public class MessyCodeRecover {
   public static void main(String[] args) throws UnsupportedEncodingException {
-    new MessyCodeRecover().recover("浣犲ソ");
+     new MessyCodeRecover().recover("浣犲ソ");
+//    System.out.println(isEnglishOrChinese("中国人abcDEF（；(,。"));
   }
 
   /**
    * 通过对常见编码格式尝试，恢复乱码文字并分析乱码原因
    * 
    * 美国：ASCII
+   * 
    * 西欧：ASCII -> ISO 8859-1(又称Latin-1) -> Windows-1252
+   * 
    * 中国：ASCII -> GB2312 -> GBK -> GB18030
+   * 
    * 台湾：ASCII -> Big5
    * 
    * Unicode二进制编码：UTF-8,UTF-16,UTF-32
    */
   public void recover(String str) throws UnsupportedEncodingException {
-    String[] charsets = new String[] {"windows-1252","GB18030","Big5","UTF-8"};
-    for(int i=0;i<charsets.length;i++) {
-      for(int j=0;j<charsets.length;j++) {
-        if(i != j) {
+    String[] charsets = new String[] {"windows-1252", "GB18030", "Big5", "UTF-8"};
+    for (int i = 0; i < charsets.length; i++) {
+      for (int j = 0; j < charsets.length; j++) {
+        if (i != j) {
           String s = new String(str.getBytes(charsets[i]), charsets[j]);
-          System.out.println("----原编码为" + charsets[j] + ",被误读为" + charsets[i]);
-          System.out.println(s);
-          System.out.println();
+
+          if (isEnglishOrChinese(s)) {
+            System.out.println("----原编码为" + charsets[j] + ",被误读为" + charsets[i]);
+            System.out.println(s);
+            System.out.println();
+          } else {
+             System.out.println("skip " + charsets[j] + " -> " + charsets[i]);
+             System.out.println(s);
+             System.out.println();
+          }
         }
       }
     }
+  }
+
+  private static boolean isEnglishOrChinese(char c) {
+    Character.UnicodeBlock ub = Character.UnicodeBlock.of(c);
+    if (ub == Character.UnicodeBlock.BASIC_LATIN
+        || ub == Character.UnicodeBlock.HALFWIDTH_AND_FULLWIDTH_FORMS
+        || ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS
+        || ub == Character.UnicodeBlock.CJK_SYMBOLS_AND_PUNCTUATION) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public static boolean isEnglishOrChinese(String strName) throws UnsupportedEncodingException {
+    char[] ch = strName.toCharArray();
+    for (int i = 0; i < ch.length; i++) {
+      char c = ch[i];
+      if (!isEnglishOrChinese(c)) {
+        return false;
+      }
+    }
+    return true;
   }
 }
