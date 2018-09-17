e61f652f72be6d0a3f92ef76d6507106843f5e50
ouzezhou <ouzezhou@xdf.cn>
2018-09-17 13:35:27 +0800

    修改目录

--- src/main/java/com/ozz/tools/file/FileTools.java
+++ src/main/java/com/ozz/tools/file/FileTools.java
@@ -101,7 +101,7 @@ public class FileTools {
   @Test
   public void test() throws IOException {
     try {
-      copyDifferentFiles("D:/Develop/tools/eclipse2/eclipse", "D:/Develop/tools/eclipse", "D:/Develop/tools/temp/eclipse", "configuration", "p2");
+      copyDifferentFiles("D:/Develop/tools/eclipse2/eclipse", "D:/Develop/tools/eclipse", "D:/Develop/tools/eclipse2/tmp", "configuration", "p2");
     } catch (Exception e) {
       e.printStackTrace();
       throw e;
